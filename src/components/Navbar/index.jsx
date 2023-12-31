082e67c290aa2ccb671289dfed8d1ab8fde505ba
Osagie Bello <osagiebello@Osagies-MacBook-Pro.local>
2023-12-31 17:12:25 +0100

    feat: add scroll position tracking and dynamic navbar background color
    - Introduce the use of state to track scroll position and dynamically update the background color of the navbar based on the scroll position and dark mode settings.

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useTheme } from '../../hooks/useTheme';
 
 const Navbar = () => {
   const { toggleDarkMode, isDark } = useTheme();
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    setScrollPosition(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <nav className={`${isDark ? 'nav' : ''}`}>
+    <nav
+      className={`${isDark ? 'nav' : ''}`}
+      style={{
+        backgroundColor:
+          scrollPosition < 400
+            ? isDark
+              ? 'var(--dark-background-color)'
+              : 'var(--color-white)'
+            : isDark
+            ? 'var(--transparent-navbar)'
+            : 'var(--dark-transparent-navbar)',
+      }}
+    >
       <div>
         <Link to='/'>Home</Link>
       </div>
@@ -19,7 +44,6 @@ const Navbar = () => {
             backgroundColor: isDark ? '#fff' : '#1111119b',
             border: isDark ? '1px solid #fff' : '1px solid #333',
             color: isDark ? '#000' : '#fff',
-            // fontSize: 16
           }}
         >
           {isDark ? 'Light Mode 💡' : 'Dark Mode 🌚'}
