468d9b11e045c9c626b04be2a6660c4fef3b8b7c
Osagie Bello <osagiebello@Osagies-MacBook-Pro.local>
2024-01-05 13:47:24 +0100

    feat: add bello's bio link at the footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,6 +8,24 @@ const Footer = () => {
     <div>
       <footer className={`${isDark ? 'footer' : ''}`}>
         <p>&copy; {currentYear} Techstack Media</p>
+        <p>
+          Made with{' '}
+          <img
+            style={{ marginBottom: '-3px' }}
+            src='https://techstackmedia.com/images/love.svg'
+            width={18}
+            alt='red love heart'
+          />{' '}
+          by{' '}
+          <a
+            href='https://bello.bio.link/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className={isDark ? 'dark-a' : ''}
+          >
+            Bello Osagie Noah
+          </a>
+        </p>
       </footer>
     </div>
   );
